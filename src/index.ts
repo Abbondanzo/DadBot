0209ab0ef25517421c4fdac8d25def58ad73f577
Abbondanzo <peter@abbondanzo.com>
2020-08-31 22:34:31 -0400

    Init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import * as TelegramBot from 'node-telegram-bot-api';
+
+config();
+
+const init = () => {
+    const token = process.env.API_KEY;
+
+    const bot = new TelegramBot(token, { polling: true });
+
+    bot.onText(/[R,r]([E,e]){3,}/, (msg, match) => {
+        const chatId = msg.chat.id;
+        const resp = match[0];
+
+        bot.sendMessage(chatId, resp);
+    });
+};
+
+init();
