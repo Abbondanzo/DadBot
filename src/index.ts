a830b3d6bd620d9508cbf12ecdea4b5c8598c8d8
Abbondanzo <peter@abbondanzo.com>
2020-09-30 01:28:50 -0400

    Some setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { config } from 'dotenv';
 import * as TelegramBot from 'node-telegram-bot-api';
 
+const goToBed = (userId: number): boolean => {
+    const currentHour = new Date().getHours();
+    const isLate = currentHour >= 0 && currentHour < 6;
+    return String(userId) === process.env.PETER_TELEGRAM_ID && isLate;
+};
+
+const logMessage = (message: string) => {
+    console.log(`${new Date().toISOString()} - ${message}`);
+};
+
 const init = () => {
     config();
 
@@ -12,9 +22,16 @@ const init = () => {
         const chatId = msg.chat.id;
         const resp = match[0];
 
-        console.log(`${new Date().toISOString()} - Sending response message: ${resp}`);
-
+        logMessage(`Received message from ${msg.from.id} ${msg.from.username}`);
+        logMessage(`Sending response message: ${resp}`);
         bot.sendMessage(chatId, resp);
+    });
+
+    bot.onText(/.*/, (msg) => {
+        if (goToBed(msg.from.id)) {
+            logMessage('Telling Peter to go to bed');
+            bot.sendMessage(msg.chat.id, 'Go to bed, Peter');
+        }
     });
 };
 
